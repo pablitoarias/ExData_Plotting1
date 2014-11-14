324df52df921d394726f6ebfb7b22a1570d83812
pablitoarias <pablo.arias@davincianhealthcare.com>
2014-11-14 14:00:22 -0600

    Added comments

--- plot3.R
+++ plot3.R
@@ -1,11 +1,20 @@
+#Read dataset
+# ns.strings = '?' identifies ? as NAs
+# colClasses imports first 2 columns as character an dthe rest as numbers
 data<-read.table("household_power_consumption.txt", 
                  header=TRUE, sep=";", na.strings="?",
-                 colClasses=c("character","character",rep("numeric",7)))
-data<-subset(data,as.Date(strptime(Date, "%d/%m/%Y"))>="2007-02-01" & 
-                     as.Date(strptime(Date, "%d/%m/%Y"))<="2007-02-02")
-data<-transform(data, DateTime=as.POSIXct(strptime(paste(data$Date,data$Time),"%d/%m/%Y %H:%M:%S")))
+                 colClasses=c("character","character",rep("numeric",7))) 
+# Extract only the dates we care about
+data<-subset(data,as.Date(strptime(Date, "%d/%m/%Y"))>="2007-02-01" 
+             & as.Date(strptime(Date, "%d/%m/%Y"))<="2007-02-02")
+# Tied Date and Time into a single Date value
+data<-transform(data, 
+                DateTime=as.POSIXct(strptime(paste(data$Date,data$Time),"%d/%m/%Y %H:%M:%S")))
+# Remove Date and Time columns
 data<-data[,!(names(data) %in% c("Date","Time"))]
+# reorder to put DateTime as first column
 data<-data[c(8,1:7)]
+#Open PNG device
 png("plot3.png",bg="transparent")
 par(mar=c(3,4,2,2))
 plot(data$DateTime,data$Sub_metering_1, type="n", ylab="Energy sub metering", xlab=NULL)
